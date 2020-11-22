c19326600891757dd3e995ea3b29982c8aeace7c
TheEpiCreator <53312465+TheEpiCreator@users.noreply.github.com>
2020-11-21 18:04:16 -0600

    Removed ts support in favor of JS, added comments

--- public/scripts/button.js
+++ public/scripts/button.js
@@ -1,13 +1,19 @@
-"use strict";
-const button = document.getElementById("download");
-const sound1 = document.getElementById("button-down");
-const sound2 = document.getElementById("button-up");
+// get download button element
+const button = document.getElementById("download")
+// get sound element
+const sound1 = document.getElementById("button-down")
+const sound2 = document.getElementById("button-up")
+
+// check if button loaded correctly
 if (button) {
-    button.addEventListener("mousedown", e => sound1 === null || sound1 === void 0 ? void 0 : sound1.play());
-    button.addEventListener("mouseup", e => setTimeout(() => sound2 === null || sound2 === void 0 ? void 0 : sound2.play(), 180));
+    // button pressed                         play press sound
+    button.addEventListener("mousedown", e => sound1?.play())
+    // Button released                                       play release sound (delayed)
+    button.addEventListener("mouseup", e => setTimeout(() => sound2?.play(), 180))
+    // Initiate download using file loading iframe
     button.addEventListener("mouseup", e => setTimeout(() => {
         // Initiate download
-        let fileLoader = document.getElementById("file-loader");
-        fileLoader ? fileLoader.src = "/download/get" : fileLoader = null;
-    }, 230));
+        let fileLoader = document.getElementById("file-loader")
+        fileLoader? fileLoader.src = "/download/get": fileLoader = null
+    }, 230))
 }
