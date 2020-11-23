f593eef9ccfffe5f39e43c580d580e47fd4f0789
TheEpiCreator <53312465+TheEpiCreator@users.noreply.github.com>
2020-11-22 18:09:03 -0600

    Performance improvements for new video

--- public/scripts/videoscroll.js
+++ public/scripts/videoscroll.js
@@ -1,8 +1,9 @@
 var viscousPos = 0
+var canplay = true
 // set scroll viscosity, 0-infinity, higher=>more viscous
 const scrollViscosity = 10
 // set fps
-const fps = 24
+const fps = 60
 const video = document.getElementById("scrollvid")
 
 const body = document.body,
@@ -12,13 +13,19 @@ const height = Math.max(body.scrollHeight, body.offsetHeight,
     html.clientHeight, html.scrollHeight, html.offsetHeight)
 
 scrollToVideo = () => {
-    let scrollPos = document.documentElement.scrollTop || document.body.scrollTop
-    // Apply viscosity to create a slowed-down smoother scroll
-    viscousPos = (scrollPos + viscousPos*scrollViscosity) / (scrollViscosity+1)
-    // Apply scroll to video
-    video.currentTime = viscousPos * video.duration / height
+    if (canplay) {
+        let scrollPos = document.documentElement.scrollTop || document.body.scrollTop
+        // Apply viscosity to create a slowed-down smoother scroll
+        viscousPos = (scrollPos + viscousPos * scrollViscosity) / (scrollViscosity + 1)
+        // Apply scroll to video
+        video.currentTime = viscousPos * video.duration / height
+    }
 }
 
 startScroll = () => {
     setInterval(scrollToVideo, 1000 / fps)
+}
+
+window.onload = () => {
+    setTimeout(startScroll, 1000)
 }
